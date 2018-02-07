0b3cc1f8c3694dccaaddae066b906f2c0c72d668
dhart001 <dhart001@users.noreply.github.com>
2018-02-06 16:00:55 -0800

    Update settings.py
    haha

--- src/todobackend/settings.py
+++ src/todobackend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8i(r%c#r131&12h0k1&+b*@@vjog9_1xd0)am8iq2*d(7axz4$'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
